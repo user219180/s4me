2883025783ca2b28e5474b80f62f80f957341c10
user219180 <155608666+user219180@users.noreply.github.com>
2024-06-22 18:35:55 +0800

    Add files via upload

--- xx.sh
+++ xx.sh
@@ -1,1 +1,13 @@
-wget https://us.download.nvidia.cn/tesla/440.95.01/NVIDIA-Linux-x86_64-440.95.01.run
+# wget https://us.download.nvidia.cn/tesla/440.95.01/NVIDIA-Linux-x86_64-440.95.01.run
+
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo add-apt-repository ppa:ubuntu-toolchain-r/ppa
+apt search gcc-12
+
+sudo apt install gcc-12 g++-12 -y
+
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-12 60 --slave /usr/bin/g++ g++ /usr/bin/g++-12
+
+g++ -v
+gcc -v
